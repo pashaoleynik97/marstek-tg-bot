5e1b5f8ba9db915667d69ed0ceff63755169da50
mthkr <oleynik.pavel.97@gmail.com>
2026-04-10 23:07:18 +0300

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ version = "1.0-SNAPSHOT"
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
